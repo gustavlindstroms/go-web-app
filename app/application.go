66e4acd0f50ad972442b294777a4e39d494cf093
Gustav Lindström <gustav.lindstrom@xlent.se>
2020-08-29 00:38:29 +0200

    yes?

---
+++ app/application.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s %s", name, name)
+	})
+
+	router.Run(":5000")
+}
