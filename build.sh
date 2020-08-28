0d4c94211619fd9e30dab5ffb0a39bf3feb3c39c
Gustav Lindström <gustav.lindstrom@xlent.se>
2020-08-29 00:56:53 +0200

    hej

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+set -xe
 
-# Install dependencies.
+# get all of the dependencies needed
 go get "github.com/gin-gonic/gin"
+go get "github.com/memcachier/mc"
+go get "github.com/gin-contrib/cache"
+go get "github.com/gin-contrib/sessions"
+go get "github.com/gin-contrib/sessions/memcached"
 
-# Build app
-go build ./ -o bin/application
+# create the application binary that eb uses
+GOOS=linux GOARCH=amd64 go build -o bin/application -ldflags="-s -w"
